60b2f1776367e89bc5a307e68b49bf3f92299180
Søren Højlund <so_hoe@hotmail.com>
2020-11-03 16:38:21 +0100

    Add project files.

---
+++ Starter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace DeckTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+            deck.Shuffle();
+
+            // TODO 2: Iterate through all cards with foreach
+            foreach (Card card in deck)
+            {
+                Console.WriteLine(card);
+            }
+            Console.WriteLine("----");
+            // TODO 4: Arrange and display Deck
+            deck.Arrange();
+            foreach (Card card in deck)
+            {
+                Console.WriteLine(card);
+            }
+        }
+    }
+}
---
+++ Starter/RankComparer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Starter
+{
+    class RankComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
